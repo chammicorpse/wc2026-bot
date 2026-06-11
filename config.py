32d99a96017a09d706942cb9fe033f6de68b8ab8
chammicorpse <chammi.corpse@gmail.com>
2026-06-11 17:54:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
-    raise ValueError("No BOT_TOKEN found! Please set it in .env file or Railway environment variables.")
+    raise ValueError("No BOT_TOKEN found!")
+
+# Google Sheets
+GOOGLE_SHEETS_KEY = os.getenv("GOOGLE_SHEETS_KEY")  # ID таблицы
